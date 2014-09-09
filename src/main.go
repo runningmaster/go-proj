3b4f0fb34bc8dcd947526c34554ef9a3d84feccf
Dmitriy Kovalenko <runningmaster@gmail.com>
2014-09-09 13:13:18 +0300

    Add ext/toml

--- src/main.go
+++ src/main.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"time" // for toml config example
 
+	"github.com/BurntSushi/toml"
 	"github.com/bradfitz/iter"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/golang/glog"
@@ -32,6 +34,51 @@ func init() {
 func main() {
 	flag.Parse()
 
+	// TOML config
+	var tomlBlob = `
+# Some comments.
+[alpha]
+ip = "10.0.0.1"
+
+	[alpha.config]
+	Ports = [ 8001, 8002 ]
+	Location = "Toronto"
+	Created = 1987-07-05T05:45:00Z
+
+[beta]
+ip = "10.0.0.2"
+
+	[beta.config]
+	Ports = [ 9001, 9002 ]
+	Location = "New Jersey"
+	Created = 1887-01-05T05:55:00Z
+`
+
+	type serverConfig struct {
+		Ports    []int
+		Location string
+		Created  time.Time
+	}
+
+	type server struct {
+		IP     string       `toml:"ip"`
+		Config serverConfig `toml:"config"`
+	}
+
+	type servers map[string]server
+
+	var config servers
+	if _, err := toml.Decode(tomlBlob, &config); err != nil {
+		glog.Fatal(err)
+	}
+
+	for _, name := range []string{"alpha", "beta"} {
+		s := config[name]
+		fmt.Printf("Server: %s (ip: %s) in %s created on %s\n",
+			name, s.IP, s.Config.Location,
+			s.Config.Created.Format("2006-01-02"))
+		fmt.Printf("Ports: %v\n", s.Config.Ports)
+	}
 	// Override glog's command line flag
 	flag.Set("log_dir", ".")
 
