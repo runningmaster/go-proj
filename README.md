33f395981c6a265e2767448eb5e5ee7dc211e843
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-07-15 02:27:09 +0300

    Add support GO15VENDOREXPERIMENT=1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Setup (in Linux)
 
 1. Copy *vars.txt* as *vars.inc* and (if necessary) override some environment settings
-2. Execute *sync-vnd* for update external dependencies (see *sync.txt*)
+2. Execute *vend-upd* for update external dependencies (see *vend.txt*)
 3. Execute *go-build* or *go-build-run*
--- src/main/main.go
+++ src/main/main.go
@@ -4,32 +4,32 @@ import (
 	"flag"
 	"fmt"
 
-	"src/version"
+	"main/internal/version"
 
 	"github.com/golang/glog"
 )
 
-var n [int32(^uint32(0) >> 1)]struct{}
+var N [int32(^uint32(0) >> 1)]struct{}
 
 func main() {
 	defer glog.Flush()
-
 	flag.Parse()
 	flag.Set("log_dir", ".") // Override glog's command line flag
 
 	fmt.Println(rangeN(5), version.Print())
 
 	// Example logging
-	if glog.V(0) {
-		glog.Info("info glog")
-		glog.Warning("warning glog")
-		glog.Error("error glog")
-	}
+	//if glog.V(0) {
+	//	glog.Info("info glog")
+	//	glog.Warning("warning glog")
+	//	glog.Error("error glog")
+	//
+	//}
 }
 
-func rangeN(N int) int {
+func rangeN(n int) int {
 	var i int
-	for i = range n[:N] {
+	for i = range N[:n] {
 	}
 	return i + 1
 }
