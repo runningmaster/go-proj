f692cb480e088e0163f14f47392f57dea44023a7
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-07-15 15:22:55 +0300

    Add iter sample

--- src/main/main.go
+++ src/main/main.go
@@ -6,9 +6,11 @@ import (
 
 	"main/internal/version"
 
+	"github.com/bradfitz/iter"
 	"github.com/golang/glog"
 )
 
+// N is alternative for bradfitz/iter
 var N [int32(^uint32(0) >> 1)]struct{}
 
 func main() {
@@ -16,20 +18,29 @@ func main() {
 	flag.Parse()
 	flag.Set("log_dir", ".") // Override glog's command line flag
 
-	fmt.Println(rangeN(5), version.Print())
+	fmt.Println(version.Print())
+	fmt.Println(len(N))
+	fmt.Println(rangeN1(5), rangeN2(5))
 
 	// Example logging
-	//if glog.V(0) {
-	//	glog.Info("info glog")
-	//	glog.Warning("warning glog")
-	//	glog.Error("error glog")
-	//
-	//}
+	if glog.V(0) {
+		glog.Info("info glog")
+		glog.Warning("warning glog")
+		glog.Error("error glog")
+
+	}
 }
 
-func rangeN(n int) int {
+func rangeN1(n int) int {
 	var i int
 	for i = range N[:n] {
+	}
+	return i + 1
+}
+
+func rangeN2(n int) int {
+	var i int
+	for i = range iter.N(n) {
 	}
 	return i + 1
 }
