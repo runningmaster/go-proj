9725d3589899ec95f6f46fd0e7b809b419faa7cf
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-07-15 18:10:15 +0300

    Fix test bench

--- src/testbench.bash
+++ src/testbench.bash
@@ -8,13 +8,13 @@ go test -bench=. -cover ./main/*.go
 printf "\n-- test internal packages --->\n"
 go test -bench=. -cover ./main/internal/...
 
-printf "\n-- lnt --->\n"
-golint ./main/*.go
-golint ./main/internal/...
-
 printf "\n-- vet --->\n"
 go vet ./main/*.go
 go vet ./main/internal/...
+
+printf "\n-- lnt --->\n"
+golint ./main/*.go
+golint ./main/internal/...
 
 printf "\n-- err --->\n"
 errcheck ./main/*.go
