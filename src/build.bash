20f9c49e911bf89214ee55af7a4ebc1907b84edd
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-07-17 02:18:27 +0300

    Minor cleanup

--- src/build.bash
+++ src/build.bash
@@ -8,11 +8,11 @@ VERSION=1.0 #`git describe --tags --abbrev=0 | sed 's/^v//' | sed 's/\+.*$$//'`
 
 go_build() {
 	go fmt main
-	go build \
-		-ldflags "\
-			-X main/internal/version.BLDTIME=$BLDTIME \
-			-X main/internal/version.GITHEAD=$GITHEAD \
-			-X main/internal/version.VERSION=$VERSION" \
+	go build -ldflags "\
+		-X main/internal/version.BLDTIME=$BLDTIME \
+		-X main/internal/version.GITHEAD=$GITHEAD \
+		-X main/internal/version.VERSION=$VERSION \
+		"\
 		-o $GOPATH/bin/$PROJ_NAME main
 	echo "OK (build)"
 }
