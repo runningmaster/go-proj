c78513c741479afb0b9966b1f5d046dcfa431d90
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-09-24 23:51:11 +0300

    Minor cleanup

--- sync.bash
+++ sync.bash
@@ -23,6 +23,13 @@ go_get_and_install() {
 		find $GOPATH/src/vendor -name "$i" -print0 | xargs -0 rm -rf
         done
 
+	# workaround for linting with GO15VENDOREXPERIMENT=1
+        rm -rf $GOPATH/pkg
+        go install main
+        for f in $GOPATH/pkg/linux_amd64/vendor/*; do
+                ln -s $f $GOPATH/pkg/linux_amd64/
+        done
+
 	echo "OK (get/install)"
 }
 
