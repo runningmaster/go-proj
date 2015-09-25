ee0cbfa399f2c80c6ad884b67c87c7764441a484
Dmitriy Kovalenko <runningmaster@gmail.com>
2015-09-25 15:10:57 +0300

    Fix sync.bash

--- sync.bash
+++ sync.bash
@@ -3,6 +3,8 @@
 . vars.inc
 
 go_get_and_install() {
+	rm -rf $GOPATH/pkg
+	rm -rf $GOPATH/src/vendor
 	while read pkg
 	do
 		[[ "$pkg" =~ ^#.*$ ]] && continue
@@ -11,7 +13,6 @@ go_get_and_install() {
 			echo "$pkg"
 			go get -d $pkg
 			pkgdir=$(echo "/$pkg" | cut -d "/" -f2)
-			rm -rf $GOPATH/src/vendor/$pkg
 			mkdir -p $GOPATH/src/vendor/$pkgdir 
 			cp -rf $GOPATH/src/$pkgdir $GOPATH/src/vendor/
 			rm -rf $GOPATH/src/$pkgdir
@@ -21,6 +22,12 @@ go_get_and_install() {
         for i in ".hg*" ".git*" ".bzr*" ".svn"
         do
 		find $GOPATH/src/vendor -name "$i" -print0 | xargs -0 rm -rf
+        done
+
+	go install main && rm -rf $GOPATH/bin/main
+	# workaround for linting with GO15VENDOREXPERIMENT=1
+        for f in $GOPATH/pkg/linux_amd64/vendor/*; do
+                ln -s $f $GOPATH/pkg/linux_amd64/
         done
 
 	echo "OK (get/install)"
