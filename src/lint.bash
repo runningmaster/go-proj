e8078a3d69f0b6987d0a43d963c8b6a7ee395045
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-02-10 22:01:26 +0200

    Remove workaround for https://github.com/golang/go/issues/14215 (fixed in go)

--- src/lint.bash
+++ src/lint.bash
@@ -3,8 +3,4 @@
 
 rm -rf $GOPATH/pkg/
 go list ./... | grep -v vendor/ | xargs -L1 go install
-go list ./... | grep -v vendor/ | xargs -L1 gometalinter --disable=gotype
-
-# workaround for https://github.com/golang/go/issues/14215
-gotype $(pwd)/internal
-gotype $(pwd)/main
+go list ./... | grep -v vendor/ | xargs -L1 gometalinter
