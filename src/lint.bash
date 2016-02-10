89fd47057c111361bb75f3072aaeef5a0077ca07
Dmitriy Kovalenko <runningmaster@gmail.com>
2016-02-10 18:14:33 +0200

    Fix previous commit (a22d8144e4374c99f94effbd2a4b86207a40beb8)

--- src/lint.bash
+++ src/lint.bash
@@ -3,7 +3,7 @@
 
 rm -rf $GOPATH/pkg/
 go list ./... | grep -v vendor/ | xargs -L1 go install
-#go list ./... | grep -v vendor/ | xargs -L1 gometalinter --disable=gotype
+go list ./... | grep -v vendor/ | xargs -L1 gometalinter --disable=gotype
 
 # workaround for https://github.com/golang/go/issues/14215
 gotype $(pwd)/internal
